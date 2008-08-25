45e1c53258c274868a7fa8f121c836113a99db1a
Kevin Smith <kevin@hypotheticalabs.com>
2008-08-25 05:30:34 -0400

    * Added transpose-buffers

--- emacs/custom_keys/custom_keys_config.el
+++ emacs/custom_keys/custom_keys_config.el
@@ -1,5 +1,7 @@
 (provide 'custom_keys_config)
 
+
+
 (defun set-colors(backgrnd foregrnd)
   (set-background-color backgrnd)
   (set-foreground-color foregrnd)
@@ -21,12 +23,26 @@
    '(default ((t (:stipple nil :background "Black" :foreground "White"
 			   :inverse-video nil :box nil :strike-through nil
 			   :overline nil :underline nil :slant normal
-			   :weight normal :height 135 :width normal
+			   :weight normal :height 120 :width normal
 			   :family "apple-monaco")))))
   (set-colors "Black" "White"))
 
+;; Switches buffers between two Emacs windows
+;; Obtained from http://www.emacswiki.org/cgi-bin/wiki?SwitchingBuffers#toc5
+(defun transpose-buffers (arg)
+  "Transpose the buffers shown in two windows."
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+            (next-win (window-buffer (funcall selector))))
+        (set-window-buffer (selected-window) next-win)
+        (set-window-buffer (funcall selector) this-win)
+        (select-window (funcall selector)))
+      (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
+
 ;; Kills all them buffers except scratch
-;; obTained From http://www.chrislott.org/geek/emacs/dotemacs.html
+;; Obtained From http://www.chrislott.org/geek/emacs/dotemacs.html
 (defun nuke-all-buffers ()
   "kill all buffers, leaving *scratch* only"
   (interactive)
@@ -36,6 +52,7 @@
 
 (global-set-key (kbd "M-w") 'toggle-colors-white)
 (global-set-key (kbd "M-b") 'toggle-colors-black)
+(global-set-key (kbd "C-x 4 t") 'transpose-buffers)
 (define-key osx-key-mode-map [home] 'beginning-of-line)
 (define-key osx-key-mode-map [end] 'end-of-line)
 (define-key osx-key-mode-map [f3] 'nuke-all-buffers)
