2619d150f8bde10c40792ac8e2ed4559f9183f2d
Kevin Smith <kevin@hypotheticalabs.com>
2008-09-03 17:58:28 -0400

    * Added git-mode

--- emacs/my_config.el
+++ emacs/my_config.el
@@ -6,6 +6,7 @@
 (add-to-list 'default-frame-alist '(alpha . (100 100)))
 (add-to-list 'load-path "~/emacs/erlang") ;; Configuration for Erlang mode
 (add-to-list 'load-path "~/emacs/flymake") ;; Flymake syntax checker
+(add-to-list 'load-path "~/emacs/git") ;; Git mode
 ;;(add-to-list 'load-path "~/emacs/distel") ;; Distel package
 (add-to-list 'load-path "~/emacs/whitespace") ;; Whitespace package
 (add-to-list 'load-path "~/emacs/autosave") ;; Autosave config
@@ -15,5 +16,6 @@
 (require 'flymake_config) ;; Loading flymake
 ;;(require 'distel_config) ;; Loading distel
 (require 'whitespace_config) ;; Loading whitespace
+(require 'git) ;; Loading Git mode
 (require 'autosave_config) ;; Configures autosaving
 (require 'custom_keys_config) ;; custom key bindings
