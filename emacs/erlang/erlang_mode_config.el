0ebccc75cacfc60331e2e2b7dbc3ac656ec64153
Kevin A. Smith <ksmith@engineyard.com>
2008-10-29 08:27:09 -0400

    * Fixed tab vs. spaces bug when other modes reset indent-tabs-mode * Taught eflymake to look for include directories

--- emacs/erlang/erlang_mode_config.el
+++ emacs/erlang/erlang_mode_config.el
@@ -2,8 +2,10 @@
 
 (add-to-list 'load-path "/opt/lib/erlang/lib/tools-2.6.1/emacs")
 (require 'erlang-start)
-(setq erlang-indent-level 2)
-(add-to-list 'exec-path "/opt/bin")
-(setq erlang-root-dir "/opt")
-
-(setq erlang-skel-mail-address "kevin@hypotheticalabs.com")
+(add-hook 'erlang-mode-hook
+	  (lambda()
+	    (setq indent-tabs-mode nil)
+	    (setq erlang-indent-level 2)
+	    (setq erlang-tab-always-indent nil)
+	    (add-to-list 'exec-path "/opt/bin")
+	    (setq erlang-root-dir "/opt")))
